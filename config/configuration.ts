87b23ccce6d6634912d8f9e9fdfd1390e1c36d50
ivanorangedigital <orangebud521@gmail.com>
2023-06-15 22:21:32 +0200

    make it better

--- config/configuration.ts
+++ config/configuration.ts
@@ -2,5 +2,5 @@ export const configuration = () => ({
     NODE_ENV: process.env.NODE_ENV,
     API_KEY: process.env.API_KEY,
     PASSWORD: process.env.PASSWORD,
-    ORIGIN: [process.env.ORIGIN, 'https://py.ristoranteclassicosalernitano.it']
+    ORIGIN: [process.env.ORIGIN, 'https://py.ristoranteclassicosalernitano.com']
 })
